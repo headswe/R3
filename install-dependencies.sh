86bcc825547ad139db6b59c19dc7fdfeb007dc62
Sebastian Nygren <headswe92@gmail.com>
2017-08-20 12:01:50 +0200

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cd extension/build
 wget https://pocoproject.org/releases/poco-1.7.5/poco-1.7.5-all.zip
 wget https://github.com/gabime/spdlog/archive/v0.10.0.zip
 wget https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-linux-glibc2.12-i686.tar.gz
@@ -9,7 +8,7 @@ tar -zxf mysql-connector-c-6.1.11-linux-glibc2.12-i686.tar.gz
 mv poco-1.7.5-all poco
 mv mysql-connector-c-6.1.11-linux-glibc2.12-i686 mysql
 mv ../Linux32-gcc poco/build/config/
-mv spdlog-0.10.0/include/spdlog ../include
+mv spdlog-0.10.0/include/spdlog extension/build/include
 cd poco
 ./configure --config=Linux32-gcc --static --shared --no-samples --no-tests --include-path=~/build/*/R3/mysql/include --library-path=~/build/*/R3/mysql/lib --omit=CppUnit,CppUnit/WinTestRunner,Net,Crypto,NetSSL_OpenSSL,NetSSL_Win,Data/SQLite,Data/ODBC,Zip,PageCompiler,PageCompiler/File2Page,PDF,CppParser,MongoDB,PocoDoc,ProGen
 make -s
